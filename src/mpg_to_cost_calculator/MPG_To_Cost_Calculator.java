650efbcc4d0e8c921b38917c8ba50c5d07ec25af
Harry Fahringer III <harryrf@users.noreply.github.com>
2021-08-02 01:01:34 -0400

    Initial commit
    Initial set up for MPG to Cost Calculator

---
+++ src/mpg_to_cost_calculator/MPG_To_Cost_Calculator.java
@@ -0,0 +1,19 @@
+package mpg_to_cost_calculator;
+
+/**
+ *
+ * @author Harry Fahringer 3rd
+ * 
+ * 1 Aug 2021
+ */
+
+
+public class MPG_To_Cost_Calculator {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+}
