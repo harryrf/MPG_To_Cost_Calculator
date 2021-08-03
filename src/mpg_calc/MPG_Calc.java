d661e126e1cd21d20b4807025ff49b32d79c5573
Harry Fahringer III <harryrf@users.noreply.github.com>
2021-08-03 01:45:03 -0400

    Update MPG_Calc.java
    Created methods that will gather information from the user and do bts work.

--- src/mpg_calc/MPG_Calc.java
+++ src/mpg_calc/MPG_Calc.java
@@ -13,30 +13,53 @@ public class MPG_Calc {
   
   // -- Assignments --
   
-  double mpg = 26.7;                 // * Make *
-  double tankSize = 12.4;             // * Into *
-  double currentPricePerGallon = 3.273; // * Query *
-  double maxMilesPerTank;                 // * Process *
-  double costPerMile; // - Fixed number per instance
+  double mpg; // 26.7
+  double tankSize; // 12.4
+  double currentPricePerGallon; // 3.273
+  double maxMilesPerTank; // mpg * tankSize
+  double costPerMile; // Fixed number per instance
   
   
   // -- Constructor --
   
-  public MPG_Calc 
-    (double _mpg, double _tankSize, double _currentPricePerGallon, 
-      double _maxMilesPerTank, double _costPerMile) {
+  public MPG_Calc (double _mpg, double _tankSize, double _currentPricePerGallon, 
+    double _maxMilesPerTank, double _costPerMile) {
       
     mpg = _mpg;
     tankSize = _tankSize;
     currentPricePerGallon = _currentPricePerGallon;
     maxMilesPerTank = _maxMilesPerTank;
-    costPerMile = _costPerMile;   
+    costPerMile = _costPerMile;
+    
   }
   
   
   // -- Methods --
+
+  public void getMPG() {
+    // Get input from user
+  }
   
+  public void getTANKSIZE() {
+    
+  }
+  
+  public void getCURRENTPRICEPERGALLON() {
+    
+  }
+  
+  public void getMAXMILESPERTANK() {
+    
+  }
+  
+  public void getCOSTPERMILE() {
+    
+  }
+  
+  
+  // Console
   public void _ConsoleOutput () {
+    
     System.out.println("----------------------------------------");
     System.out.println("mpg: " + mpg);
     System.out.println("tankSize: " + tankSize);
@@ -44,14 +67,18 @@ public class MPG_Calc {
     System.out.println("maxMilesPerTank:" + maxMilesPerTank);
     System.out.println("costPerMile:" + costPerMile);
     System.out.println("----------------------------------------");
+    
   }
   
   
   // -- # Main Function
 
-  public static void main(String[] args) {
-  //  _ConsoleOutput();
+  public static void main (String[] args) {
   
+  // New instance to use with dialog/interface
+  MPG_Calc newQuery = new MPG_Calc(0, 0, 0, 0, 0);
+  
+  newQuery._ConsoleOutput();
   
   }
 
