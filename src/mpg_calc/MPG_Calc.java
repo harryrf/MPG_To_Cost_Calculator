9a3d614c173c6f48005a157af610d1a0d4f6c16b
Harry Fahringer III <harryrf@users.noreply.github.com>
2021-08-05 16:13:07 -0400

    Big changes were made to get user input.

--- src/mpg_calc/MPG_Calc.java
+++ src/mpg_calc/MPG_Calc.java
@@ -14,80 +14,39 @@ import java.util.Scanner;
 
 public class MPG_Calc {
 
-  // creates a new instance
-  public static final Scanner in = new Scanner(System.in);
-  
-  // -- Assignments --
-  
-  double mpg; // 26.7
-  double tankSize; // 12.4
-  double currentPricePerGallon; // 3.273
-  double maxMilesPerTank; // mpg * tankSize
-  double costPerMile; // Fixed number per instance
-  
-  
-  // -- Constructor --
-  
-  public MPG_Calc (double _mpg, double _tankSize, double _currentPricePerGallon, 
-    double _maxMilesPerTank, double _costPerMile) {
-    
-    mpg = _mpg;
-    tankSize = _tankSize;
-    currentPricePerGallon = _currentPricePerGallon;
-    maxMilesPerTank = _maxMilesPerTank;
-    costPerMile = _costPerMile;
-    
-  }
-  
-  
-  // -- Methods --
-
-  public void getMPG() {
-    // * Get input from user
-  }
-  
-  public void getTANKSIZE() {
-    // *
-  }
-  
-  public void getCURRENTPRICEPERGALLON() {
-    // *
-  }
-  
-  public void getMAXMILESPERTANK() {
-    // *
-  }
-  
-  public void getCOSTPERMILE() {
-    // *
-  }
-  
-  
-  // Console
-  public void _ConsoleOutput () {
-    
-    System.out.println("----------------------------------------");
-    System.out.println("mpg: " + mpg);
-    System.out.println("tankSize: " + tankSize);
-    System.out.println("currentPricePerGallon: " + currentPricePerGallon);
-    System.out.println("maxMilesPerTank: " + maxMilesPerTank);
-    System.out.println("costPerMile: " + costPerMile);
-    System.out.println("----------------------------------------");
-    
-  }
-  
-  
   // -- Main Function --
 
   public static void main (String[] args) {
-  
-  // New instance to use with dialog/interface
-  MPG_Calc newQuery = new MPG_Calc(0, 0, 0, 0, 0);
-  
-  newQuery._ConsoleOutput();
-  
-  }
 
+  // Open the Scanner utility so we can receive user input
+  Scanner in = new Scanner(System.in);
+  
+  // Tell the user what input is needed
+  System.out.println("Please enter your current miles per gallon:");
+
+  double mpg = in.nextDouble(); // 26.7
+
+  System.out.println("Please enter the size of your gas tank (gallons):");
+
+  double tankSize = in.nextDouble(); // 12.4
+
+  System.out.println("Please enter the current price of gas:");
+
+  double currentPricePerGallon = in.nextDouble(); // 3.279
+
+  double maxMilesPerTank = mpg * tankSize; // 331.08
+
+  double fullTankPrice = currentPricePerGallon * tankSize; // 40.6596 
+
+  double costPerMile = maxMilesPerTank / fullTankPrice; // 8.14 cents 
+
+  System.out.println("Your cost per mile is " + costPerMile);
+  System.out.println("Your price for a full tank of gas is $" + fullTankPrice);
+  System.out.println("You can go an estimated " + maxMilesPerTank + " miles with a full tank of gas.");
+
+  //closes scanner
+  in.close();
+  }
 
 
 } //#-
